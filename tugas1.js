f73644ae9f19198d483cc5f52dc18ebbaa993224
puputrizka <72289618+puputrizka@users.noreply.github.com>
2021-01-24 09:29:35 +0700

    Mengerjakan soal logic 1 (#1)

---
+++ tugas1.js
@@ -0,0 +1,43 @@
+// Soal No. 1
+
+const string1 = "hello";
+const string2 = "1+1";
+const sum = 1+1;
+const boolean1 = true;
+const boolean2 = false;
+
+// Soal No. 2
+
+console.log(string1);
+console.log(string2);
+console.log(sum);
+console.log(boolean1);
+console.log(boolean2);
+
+// Soal No. 3
+
+function a() {
+    return string1;
+}
+console.log(a())
+
+function b() {
+    return string2;
+}
+console.log(b());
+
+function c() {
+    return sum;
+}
+console.log(c());
+
+function d() {
+    return boolean1;
+}
+console.log(d());
+
+function e() {
+    return boolean2;
+}
+console.log(e());
+
